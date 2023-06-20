b141e771f1239c54c8255c248e914b39ab168619
Nebil Arega <36981187+nebilarega@users.noreply.github.com>
2023-06-20 11:08:44 +0300

    Changes requested

--- src/components/NonCanvas.tsx
+++ src/components/NonCanvas.tsx
@@ -99,12 +99,14 @@ export const NonCanvas: React.FC<Props> = ({
   roughnessMap.wrapS = roughnessMap.wrapT = THREE.RepeatWrapping;
   roughnessMap.repeat.set(repeatX, repeatY);
 
-  const { camera, scene, raycaster, mouse, gl, size } = useThree();
+  const { camera, scene, raycaster, mouse, gl } = useThree();
+  // const [manualClose, setManualClose] = useState(false);
   const previousMousePosition = useRef<[number, number]>([mouse.x, mouse.y]);
   const originalPosition = useRef(camera.position.clone());
   const originalRotation = useRef(camera.rotation.clone());
   const originalFOV = useRef((camera as THREE.PerspectiveCamera).fov);
   const currentObject = useRef<THREE.Object3D | null>(null);
+  const manualClose = useRef<boolean>(false);
 
   const resetCamera = () => {
     new TWEEN.Tween(camera.position)
@@ -269,64 +271,59 @@ export const NonCanvas: React.FC<Props> = ({
   const handleDomClick = (event: Event) => {
     raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects(scene.children, true);
-    if (!isDomDragged) {
-      //   const enableOrbitControl =
-      //     intersects.length === 0 ||
-      //     !intersects.some((intersect) => intersect.object.type === "Mesh");
 
+    if (!isDomDragged) {
       if (intersects.length > 0) {
         const int_obj = intersects[0].object;
         if (
           !["Stand", "Barb", "floor"].includes(int_obj.name) &&
           !int_obj.name.includes("SROL")
         ) {
-          // createBoundingBox(int_obj);
-
           if (
             int_obj.parent &&
             int_obj.parent.name !== "Scene" &&
             int_obj.parent.type !== "Scene"
           ) {
-            createBoundingBoxHelper(
-              int_obj.parent,
-              0xff0000,
-              1,
-              scene,
-              dataMap,
-              "clicked"
-            );
-            if (int_obj.parent.name.includes("SC_2")) {
-              hideBlocks("SC_1", scene, dataMap);
-            }
-            if (int_obj.parent.name.includes("SC_3")) {
-              hideBlocks("SC_1", scene, dataMap);
-              hideBlocks("SC_2", scene, dataMap);
-            }
-            // createBoundingBox(int_obj.parent, 0xff0000, scene);
-            // camera.lookAt(
-            //   int_obj.parent.position.add(
-            //     new THREE.Vector3(mouse.x, mouse.y, 0)
-            //   )
-            // );
-            const startPosition = camera.position.clone();
-            const endPosition = intersects[0].point
-              .clone()
-              .add(new THREE.Vector3(-1.5, 0, 2));
+            if (manualClose.current) {
+              setCloseClicked(true);
+              setCloseVisible(false);
+            } else {
+              createBoundingBoxHelper(
+                int_obj.parent,
+                0xff0000,
+                1,
+                scene,
+                dataMap,
+                "clicked"
+              );
+              if (int_obj.parent.name.includes("SC_2")) {
+                hideBlocks("SC_1", scene, dataMap);
+              }
+              if (int_obj.parent.name.includes("SC_3")) {
+                hideBlocks("SC_1", scene, dataMap);
+                hideBlocks("SC_2", scene, dataMap);
+              }
+              const startPosition = camera.position.clone();
+              const endPosition = intersects[0].point
+                .clone()
+                .add(new THREE.Vector3(-1.5, 0, 2));
 
-            // Create a Tween for the camera's position
-            new TWEEN.Tween(startPosition)
-              .to(endPosition, 1000) // Set the duration of the animation in milliseconds
-              .easing(TWEEN.Easing.Quadratic.InOut) // Set the easing function for the animation
-              .onUpdate(() => {
-                camera.position.copy(startPosition);
-                camera.lookAt(intersects[0].point);
-              })
-              .start();
+              // Create a Tween for the camera's position
+              new TWEEN.Tween(startPosition)
+                .to(endPosition, 1000) // Set the duration of the animation in milliseconds
+                .easing(TWEEN.Easing.Quadratic.InOut) // Set the easing function for the animation
+                .onUpdate(() => {
+                  camera.position.copy(startPosition);
+                  camera.lookAt(intersects[0].point);
+                })
+                .start();
 
-            (camera as THREE.PerspectiveCamera).fov = 25;
-            // camera.position.set(-1, 1, 3);
-            setCloseVisible(true);
-            camera.updateProjectionMatrix();
+              (camera as THREE.PerspectiveCamera).fov = 25;
+              // camera.position.set(-1, 1, 3);
+              setCloseVisible(true);
+              camera.updateProjectionMatrix();
+            }
+            manualClose.current = !manualClose.current;
           }
         } else {
           removeExistingBoundingBox(scene);
@@ -354,141 +351,117 @@ export const NonCanvas: React.FC<Props> = ({
     isDomDragged = true;
     raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects(scene.children, true);
-    if (isMovable) {
-      removeExistingBoundingBox(scene);
-      if (
-        currentObject.current &&
-        !["Stand", "Barb", "floor"].includes(currentObject.current.name)
-      ) {
-        const int_obj = currentObject.current;
-        // createBoundingBox(int_obj);
-        const parent = int_obj.parent;
-        if (parent) {
-          const collectionName = Object.keys(dataMap.collections).find(
-            (value) => parent.name.includes(value)
-          );
-          if (collectionName) {
-            const value =
-              dataMap.collections[
-                collectionName as keyof typeof dataMap.collections
-              ];
+    if (!manualClose.current) {
+      if (isMovable) {
+        removeExistingBoundingBox(scene);
+        if (
+          currentObject.current &&
+          !["Stand", "Barb", "floor"].includes(currentObject.current.name)
+        ) {
+          const int_obj = currentObject.current;
+          const parent = int_obj.parent;
+          if (parent) {
+            const collectionName = Object.keys(dataMap.collections).find(
+              (value) => parent.name.includes(value)
+            );
+            if (collectionName) {
+              const value =
+                dataMap.collections[
+                  collectionName as keyof typeof dataMap.collections
+                ];
 
-            const mapVal =
-              dataMap.maps[collectionName as keyof typeof dataMap.maps];
-            const pointIntersect =
-              intersects.length > 0 && intersects[0].point.x;
-            if (
-              mapVal.position &&
-              (mapVal.position.x < mapVal.max ||
-                mapVal.position.x > mapVal.min) &&
-              pointIntersect
-            ) {
-              value.forEach((groupName: any) => {
-                const obj = scene.getObjectByName(groupName);
-                if (obj?.position) {
-                  if (
-                    pointIntersect > obj.position.x &&
-                    mapVal.position.x < mapVal.max
-                  ) {
-                    // if (mapVal.left) {
-                    //   const left = dataMap.maps[mapVal.left as keyof typeof dataMap.maps];
-                    //   left.max = obj.position.x - modelProps.offset;
-                    // }
-                    const distance = pointIntersect - obj.position.x;
-                    const dampingFactor = 0.3; // Adjust this value to control the damping effect
-                    const newPosition =
-                      obj.position.x + distance * dampingFactor;
-                    obj.position.x = newPosition;
-                  } else if (
-                    pointIntersect < obj.position.x &&
-                    mapVal.position.x > mapVal.min
-                  ) {
-                    // if (mapVal.right) {
-                    //   const right = dataMap.maps[mapVal.right as keyof typeof dataMap.maps];
-                    //   right.min = obj.position.x + modelProps.offset;
-                    // }
-                    const distance = pointIntersect - obj.position.x;
-                    const dampingFactor = 0.3; // Adjust this value to control the damping effect
-                    const newPosition =
-                      obj.position.x + distance * dampingFactor;
-                    obj.position.x = newPosition;
+              const mapVal =
+                dataMap.maps[collectionName as keyof typeof dataMap.maps];
+              const pointIntersect =
+                intersects.length > 0 && intersects[0].point.x;
+              if (
+                mapVal.position &&
+                (mapVal.position.x < mapVal.max ||
+                  mapVal.position.x > mapVal.min) &&
+                pointIntersect
+              ) {
+                value.forEach((groupName: any) => {
+                  const obj = scene.getObjectByName(groupName);
+                  if (obj?.position) {
+                    if (
+                      pointIntersect > obj.position.x &&
+                      mapVal.position.x < mapVal.max
+                    ) {
+                      const distance = pointIntersect - obj.position.x;
+                      const dampingFactor = 0.3; // Adjust this value to control the damping effect
+                      const newPosition =
+                        obj.position.x + distance * dampingFactor;
+                      obj.position.x = newPosition;
+                    } else if (
+                      pointIntersect < obj.position.x &&
+                      mapVal.position.x > mapVal.min
+                    ) {
+                      const distance = pointIntersect - obj.position.x;
+                      const dampingFactor = 0.3; // Adjust this value to control the damping effect
+                      const newPosition =
+                        obj.position.x + distance * dampingFactor;
+                      obj.position.x = newPosition;
+                    }
+                    if (mapVal.left) {
+                      const left =
+                        dataMap.maps[mapVal.left as keyof typeof dataMap.maps];
+                      left.max = obj.position.x - modelProps.offset;
+                    }
+                    if (mapVal.right) {
+                      const right =
+                        dataMap.maps[mapVal.right as keyof typeof dataMap.maps];
+                      right.min = obj.position.x + modelProps.offset;
+                    }
                   }
-                  if (mapVal.left) {
-                    const left =
-                      dataMap.maps[mapVal.left as keyof typeof dataMap.maps];
-                    left.max = obj.position.x - modelProps.offset;
-                  }
-                  if (mapVal.right) {
-                    const right =
-                      dataMap.maps[mapVal.right as keyof typeof dataMap.maps];
-                    right.min = obj.position.x + modelProps.offset;
-                  }
-                  // if (obj.position.x >= mapVal.max)
-                  //   obj.position.x = pointIntersect;
-                }
-              });
-              mapVal.position.x = pointIntersect;
-              // if (mapVal.right) {
-              //   if (mapVal.max <= pointIntersect){
-
-              //   }
-              // }
-
-              // if (mapVal.left) {
-              //   const left = maps[mapVal.left as keyof typeof maps];
-              //   left.max += 0.02;
-              // }
+                });
+                mapVal.position.x = pointIntersect;
+              }
             }
           }
-        }
-      } else if (
-        intersects.length === 0 ||
-        (currentObject.current &&
-          ["floor"].includes(currentObject.current.name))
-      ) {
-        // camera.rotation.y -= event.movementX * 0.001 * 10;
-        // camera.rotation.x -= event.movementY * 0.001 * 10;
-        // camera.position.x = -5 * Math.sin(-event.clientX * 0.1);
-        // camera.position.z = -5 * Math.cos(-event.clientX * 0.1);
-        // camera.lookAt(new THREE.Vector3(0, 0, 0));
-        const [prevX, prevY] = previousMousePosition.current;
-        const cameraPosition = camera.position.clone();
-        const cameraRadius = cameraPosition.length();
-        const spherical = new THREE.Spherical().setFromVector3(cameraPosition);
-        spherical.theta -= (mouse.x - prevX) * 1.5;
-        spherical.phi += (mouse.y - prevY) * 1.5;
-
-        spherical.phi = Math.max(0, Math.min(Math.PI / 2, spherical.phi));
-
-        camera.position.setFromSpherical(spherical);
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
-        previousMousePosition.current = [mouse.x, mouse.y];
-      }
-    } else {
-      isMovable = false;
-      if (intersects.length > 0) {
-        const int_obj = intersects[0].object;
-        if (
-          !["Stand", "Barb", "floor"].includes(int_obj.name) &&
-          !int_obj.name.includes("SROL")
+        } else if (
+          intersects.length === 0 ||
+          (currentObject.current &&
+            ["floor"].includes(currentObject.current.name))
         ) {
-          // createBoundingBox(int_obj);
+          const [prevX, prevY] = previousMousePosition.current;
+          const cameraPosition = camera.position.clone();
+          const cameraRadius = cameraPosition.length();
+          const spherical = new THREE.Spherical().setFromVector3(
+            cameraPosition
+          );
+          spherical.theta -= (mouse.x - prevX) * 1.5;
+          spherical.phi += (mouse.y - prevY) * 1.5;
 
+          spherical.phi = Math.max(0, Math.min(Math.PI / 2, spherical.phi));
+
+          camera.position.setFromSpherical(spherical);
+          camera.lookAt(new THREE.Vector3(0, 0, 0));
+          previousMousePosition.current = [mouse.x, mouse.y];
+        }
+      } else {
+        isMovable = false;
+        if (intersects.length > 0) {
+          const int_obj = intersects[0].object;
           if (
-            int_obj.parent &&
-            int_obj.parent.name !== "Scene" &&
-            int_obj.parent.type !== "Scene"
+            !["Stand", "Barb", "floor"].includes(int_obj.name) &&
+            !int_obj.name.includes("SROL")
           ) {
-            createBoundingBoxHelper(
-              int_obj.parent,
-              0x1a872e,
-              1,
-              scene,
-              dataMap
-            );
+            if (
+              int_obj.parent &&
+              int_obj.parent.name !== "Scene" &&
+              int_obj.parent.type !== "Scene"
+            ) {
+              createBoundingBoxHelper(
+                int_obj.parent,
+                0x1a872e,
+                1,
+                scene,
+                dataMap
+              );
+            }
+          } else {
+            removeExistingBoundingBox(scene);
           }
-        } else {
-          removeExistingBoundingBox(scene);
         }
       }
     }
@@ -496,8 +469,6 @@ export const NonCanvas: React.FC<Props> = ({
   const handleMouseUp = (event: Event) => {
     isMovable = false;
     currentObject.current = null;
-    const cursor = document.getElementById("content__container");
-    if (cursor) cursor!.style.cursor = "default";
   };
 
   const handleMouseMoveThrottled = useThrottle(
